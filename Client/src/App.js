2867c45cc39a0835f1551ea9e3043a777652ecb8
pranav k <pranavkkeloth@gmail.com>
2023-08-11 11:49:36 +0530

    feat: Project initialized

---
+++ Client/src/App.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import Header from './Components/Header/Header'
+import ProductList from './Components/ProductList/ProductList'
+
+
+function App() {
+  return (
+    <Fragment>
+      
+      <Header/>
+      <ProductList/>
+    
+    </Fragment>
+  );
+}
+
+export default App;
