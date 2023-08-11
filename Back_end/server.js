b8add6ea6a6051135d74ea9ba8fadb52fbafd277
pranav k <pranavkkeloth@gmail.com>
2023-08-11 12:15:47 +0530

    feat: back end connected

---
+++ Back_end/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+const app = express();
+
+//configure the .env
+dotenv.config();
+
+//database connection
+mongoose.set("strictQuery", true);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//port connection
+app.listen(8800, () => {
+    connect();
+    console.log("Backend server is running!");
+  });
+  
